a804174093409d2175d2e0bd4187af769d353e20
wbgl-blip <wbgl@live.com>
2025-12-28 01:25:49 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -167,8 +167,8 @@ if (r === "7") {
   return;
 }
 
-setTurn(t => (t + 1) % PLAYERS.length);
-
+  }setTurn(t => (t + 1) % PLAYERS.length);
+}
   /* =========================
      RACES
   ========================= */
